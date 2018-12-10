34515b7e0815e4973a83ec326ccc0679af3c48cc
Jordan HERENG <hereng.jordan@outlook.fr>
2018-12-10 20:00:50 +0100

    main and gitignore

--- Interface.py
+++ Interface.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import tkinter as tk
 from Jeu import Jeu, est_entier
 
---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from Interface import Modele
+
+def main():
+    """None -> None
+    Fonction principale."""
+    m = Modele()
+
+if __name__ == "__main__":
+    main()
